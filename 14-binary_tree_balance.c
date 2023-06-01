512c79dca2bd1b0dd409e9d284776384847d1cec
milemotigo <mtigo3617@gmail.com>
2023-06-01 13:00:37 +0100

    Task 13

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - function that measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	
+}
