fd84acd1dc9a00f45d88e1f4bdc0494358366143
Milemotigo <mtigo@gmail.com>
2023-06-02 05:58:00 -0700

    hot fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,11 +17,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (parent->left)
+	if (!parent->left)
+		parent->left = new_node;
+	else
 	{
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
-	parent->left = new_node;
 	return (new_node);
 }
