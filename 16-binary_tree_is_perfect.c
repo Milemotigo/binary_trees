b3e594bb57624840fe4fa671eae101129c0bdf5c
milemotigo <mtigo3617@gmail.com>
2023-06-02 05:49:09 +0100

    New

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,7 +10,7 @@
     if (tree == NULL)
         return (0);
     if (tree->right != NULL && tree->left != NULL)
-	    return (0);
+	    return (1);
     else
-    return(binary_tree_is_perfect(tree->left) * binary_tree_is_perfect(tree->right));
+    return(binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
